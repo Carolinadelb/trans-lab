67855175ca57410833a36edbe94f7be1e2a2d8bd
Carolinadelb <carolinadelgadob@gmail.com>
2018-06-27 16:50:15 -0400

    A

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-/* Holiii acá va tu código también */
+/* login */
+let go = () => { //solo funciona con este nombre y contrasela
+  if (document.form.password.value ==='12345678' && document.form.login.value ==='usuario@mail.com'){ 
+          window.open('home.html') //abre página index si se pone correcto
+
+      } 
+      else{ 
+           alert("Porfavor ingrese, nombre de usuario y contraseña correctos."); 
+      } 
+  } 
